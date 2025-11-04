9120345f3a3e1d677bd5e21b8f5070bdb1fcb6ac
Mostafa Fatouh <Mostafa.Fatouh@emsi-edu.ma>
2025-11-04 01:19:59 +0100

    Création de la classe villeOuPays

---
+++ src/main/java/ma/emsi/fatouh/tp3fatouh/resources/GuideTouristiqueResource.java
@@ -0,0 +1,22 @@
+package ma.emsi.fatouh.tp3fatouh.resources;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import ma.emsi.fatouh.tp3fatouh.llm.LlmClientPourGemini;
+
+@Path("/guide")
+public class GuideTouristiqueResource {
+
+    @Inject
+    LlmClientPourGemini llm;
+
+    @GET
+    @Path("lieu/{ville_ou_pays}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response villeOuPays(@PathParam("ville_ou_pays") String lieu) {
+        return Response.ok(lieu).build();
+    }
+}
+
